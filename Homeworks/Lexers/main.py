b13924ea6a26c8288e2dba81a200ef6a9d9810c8
Hristea Mircea <hristeamircea@yahoo.com>
2023-12-22 18:52:44 +0200

    Added calculator

---
+++ Homeworks/Lexers/main.py
@@ -0,0 +1,96 @@
+from math import factorial
+
+from sly import Lexer, Parser
+
+class CalcLexer(Lexer):
+    tokens = { NAME, NUMBER, PLUS, MINUS, MUL, DIV, ASSIGN, LPAR, RPAR, POW, FACT}
+    ignore = ' \t'
+
+    # Tokens
+    NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    NUMBER = r'\d+'
+
+    # Special symbols
+    PLUS = r'\+'
+    MINUS = r'-'
+    MUL = r'\*'
+    DIV = r'/'
+    LPAR = r'\('
+    RPAR = r'\)'
+    POW = r'\^'
+    FACT = r'\!'
+    # Ignored pattern
+    ignore_newline = r'\n+'
+
+    # Extra action for newlines
+    def ignore_newline(self, t):
+        self.lineno += t.value.count('\n')
+
+    def error(self, t):
+        print("Illegal character '%s'" % t.value[0])
+        self.index += 1
+
+class CalcParser(Parser):
+    tokens = CalcLexer.tokens
+
+    def __init__(self):
+        self.names = { }
+
+    @_('NAME ASSIGN expr')
+    def statement(self, p):
+        self.names[p.NAME] = p.expr
+
+    @_('expr')
+    def statement(self, p):
+        print(p.expr)
+
+    @_('expr PLUS expr')
+    def expr(self, p):
+        return p.expr0 + p.expr1
+
+    @_('expr MINUS expr')
+    def expr(self, p):
+        return p.expr0 - p.expr1
+
+    @_('expr MUL expr')
+    def expr(self, p):
+        return p.expr0 * p.expr1
+
+    @_('expr DIV expr')
+    def expr(self, p):
+        return p.expr0 / p.expr1
+
+    @_('LPAR expr RPAR')
+    def expr(self, p):
+        return p.expr
+
+    @_('expr POW expr')
+    def expr(self, p):
+        return p.expr0 ** p.expr1
+
+    @_('expr FACT')
+    def expr(self, p):
+        return factorial(p.expr)
+
+    @_('NUMBER')
+    def expr(self, p):
+        return int(p.NUMBER)
+
+    @_('NAME')
+    def expr(self, p):
+        try:
+            return self.names[p.NAME]
+        except LookupError:
+            print(f'Undefined name {p.NAME!r}')
+            return 0
+
+if __name__ == '__main__':
+    lexer = CalcLexer()
+    parser = CalcParser()
+    while True:
+        try:
+            text = input('calc > ')
+        except EOFError:
+            break
+        if text:
+            parser.parse(lexer.tokenize(text))
